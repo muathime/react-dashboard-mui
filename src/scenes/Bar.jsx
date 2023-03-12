dd114ffef9772f5161d1756f55877367f2398ebd
Antony <muathime@Antonys-MacBook-Pro.local>
2023-03-12 17:38:45 +0300

    nivo barchart

--- src/scenes/Bar.jsx
+++ src/scenes/Bar.jsx
@@ -1,10 +1,17 @@
+import { Box } from '@mui/material';
 import React from 'react'
+import BarChart from '../components/BarChart';
 import Header from '../components/Header'
 
 function Bar() {
   return (
-    <Header title={"Bar Chart"} subtitle={"Data presentation in Bar Chart"} />
-  )
+    <Box>
+      <Header title={"Bar Chart"} subtitle={"Data presentation in Bar Chart"} />
+      <Box height={"75vh"}>
+        <BarChart />
+      </Box>
+    </Box>
+  );
 }
 
 export default Bar
